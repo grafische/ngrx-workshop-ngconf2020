3a786ab378323bc9bf0ec7e025c1617d577f7d9c
grafweb <grafweb@wp.pl>
2020-07-01 13:52:09 +0200

    first action

--- src/app/books/actions/books-page.actions.ts
+++ src/app/books/actions/books-page.actions.ts
@@ -1,2 +1,42 @@
 import { createAction, props } from "@ngrx/store";
 import { BookRequiredProps } from "src/app/shared/models";
+
+
+// [Books Page] Create a Book
+// - BookRequiredProps
+
+export const enter = createAction('[Books Page] Enter');
+
+export const selectBook = createAction(
+    '[Books Page] Select Book',
+    props<{ bookId: string }>()
+);
+
+export const clearSelectedBook = createAction(
+    '[Books Page] Clear Selected Book'
+);
+
+export const createBook = createAction(
+    '[Books Page] Create a Book',
+    props<{ book: BookRequiredProps }>()
+);
+
+export const updateBook = createAction(
+    '[Books Page] Update a Book',
+    props<{ bookId: string, changes: BookRequiredProps }>()
+);
+
+export const deleteBook = createAction(
+    '[Books Page] Delete a Book',
+    props<{ bookId: string }>()
+);
+
+// [Books Page] Update a Book
+// - BookRequiredProps
+// - Id of the book being edited
+
+
+// [Books Page] Delete a Book
+// - Id of the book being edited
+
+// [Books Page] Cancel Editing a Book
